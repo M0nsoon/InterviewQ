db75716be9b75c41b27682c30ee40e2c6f33df8d
deptrai <muelleimer9009@gmail.com>
2019-05-15 16:31:38 +0200

    Day Six Failed: Java does not have a Pointer for Location

--- src/main/scala/DayFour/FindTheMissing.scala
+++ src/main/scala/DayFour/FindTheMissing.scala
@@ -1,9 +1,12 @@
 package DayFour
 
+import java.util
+
 class FindTheMissing {
   def ShowTheMissing(arr : Array[Int]):Int ={
     val dank = arr.toSet[Int].filter(_>0)
-    val dankE = dank.toList
+    val dankE = dank.toArray
+    scala.util.Sorting.quickSort(dankE)
     for(i <- 0 until dankE.length) {
       if((dankE(i)-dankE(i-1)) > 1) return dankE(i-1) +1
     }
