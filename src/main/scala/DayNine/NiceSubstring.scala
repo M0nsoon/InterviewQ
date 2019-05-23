03001af80267b3033e541a88e66746fc2a9422f4
deptrai <muelleimer9009@gmail.com>
2019-05-23 10:41:03 +0200

    Day Nine Done

--- src/main/scala/DayNine/NiceSubstring.scala
+++ src/main/scala/DayNine/NiceSubstring.scala
@@ -8,22 +8,61 @@ class NiceSubstring {
 For example, given s = "abcba" and k = 2,
  the longest substring with k distinct characters is "bcb".
    */
-  def findTheSubstring(str: String, k:Int):String ={
+ val maxchar = 26;
 
-  if(str.foreach(x => (str.count(_ ==x))) != k) "This String is not Eligible"
-  else {
-    //find the char with k times appearance
-    val charr = str.filter(x => str.filter(_ ==x) ==k)
-    var index = 0;
-    var temp =0
-    for(i<- 0 until str.length){
-      //find the k-th appearance of the char in element
-      if((str(i) == charr)&&(index==k)) {
-        index +=1
-      temp = i}
+  def isValid(count: Array[Int],k: Int) :Boolean ={
+    var counter = 0
+    for(i<- 0 until maxchar) {
+      if((count(i) > 0) ){
+        counter += 1
+      }
     }
-    //cut the original string from the first appearance to the k-th appearance
-    str.slice(str.indexOf(charr),temp)
+    k>=counter
   }
+  def kUniques(str:String, k: Int): Unit = {
+    var u = 0 // number of unique characters
+    val n = str.length
+    //associate array to store count
+    val count = Array.fill(maxchar)(0)
+    //traverse the string, fills the associate arrays
+    //count the number of unique characters
+    for(i<- 0 until n) {
+      if(count(str(i)-'a')==0) {
+        u += 1
+      }
+      count(str(i)-'a') += 1
+    }
+    //if not enough unique chars, returns error
+    if(u < k) {
+      println("not enough uniqe chars")
+      return
+    }
+    //otherwise take a window with first elemsin it
+    //start and end vars
+    var current_start = 0
+    var current_end = 0
+    //values for result longest windows
+    var max_window_size = 1
+    var max_window_start = 0
+    //put the first char, start from the second char,
+    //add chars in windows
+    count(str(0)-'a') += 1
+    for(i<- 0 until n) {
+      //add char s(i) to current window
+      count(str(i) -'a') += 1
+      current_end += 1
+      //if more than k unique chars in current windows,
+      //remove from the left side
+      while (!isValid(count,k)) {
+        count(str(current_start)-'a') -= 1
+        current_start +=1
+      }
+
+      //update max size if required
+      if(current_end - current_start +1 > max_window_size) {
+        max_window_size = current_end - current_start + 1
+        max_window_start = current_start
+      }
+    }
   }
 }
